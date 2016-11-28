ac2df9f8345c3cf4b1c06384f0e06bcff9c58583
jjusianiec <jax1994@gmail.com>
2016-11-29 00:41:31 +0100

    JG-50 Add returning issues with parent keys

--- src/service/issueService.ts
+++ src/service/issueService.ts
@@ -1,23 +1,54 @@
 import {JqlService} from "./jqlService";
 import {JqlModel} from "../model/jqlModel";
+import {Dictionary} from "../commons/dictionary";
+import {CardWebModel} from "../model/cardWebModel";
+import {JqlToCardWebModel} from "../converter/jqlToCardWebModel";
 /**
  * Created by JJax on 28.11.2016.
  */
 
 export class IssueService {
     private jqlService = new JqlService();
+    private jqlToCardWebModel = new JqlToCardWebModel();
 
-    getIssuesForForIds(id: number[], epicCustomField: string, httpClient): Promise<any> {
+    public async getIssuesWithParentKeys(id: number[], epicCustomField: string, httpClient)
+    : Promise<Dictionary<CardWebModel[]>> {
+        var issueBody = await this.getIssuesForIds(id, epicCustomField, httpClient);
+        var toReturn: Dictionary<CardWebModel[]> = {};
+
         return new Promise<any>((resolve, reject) => {
-            console.log(this.prepareJqlRequestForIssues(id, epicCustomField));
+            for(let issue of issueBody.issues){
+                var issueCard: CardWebModel = this.jqlToCardWebModel.apply(issue);
+                var parentId: number = this.getParentKey(issue, epicCustomField);
+                if(!parentId){
+                    reject("No parent id error");
+                }
 
+                if(!toReturn[parentId]){
+                    toReturn[parentId] = [];
+                }
+                toReturn[parentId].push(issueCard);
+            }
+            resolve(toReturn);
         });
-        // jqlService.doRequest(, httpClient)
+    }
+
+    private getParentKey(issue, customFieldName): number {
+        if(issue.fields.parent){
+            return issue.fields.parent.key;
+        } else if(issue.fields[customFieldName]) {
+            return issue.fields[customFieldName];
+        }
+    }
+
+    private getIssuesForIds(id: number[], epicCustomField: string, httpClient): Promise<any> {
+        var request = this.prepareJqlRequestForIssues(id, epicCustomField);
+        return this.jqlService.doRequest(request, httpClient);
     }
 
     private prepareJqlRequestForIssues(id: number[], epicCustomField: string): JqlModel {
         return {
-            request: `"Epic Link" in (` + id +`) OR parent in (` + id +`) ORDER BY created`,
+            request: `"Epic Link" in (` + id + `) OR parent in (` + id + `) ORDER BY created`,
             fields: [
                 "summary",
                 "description",
