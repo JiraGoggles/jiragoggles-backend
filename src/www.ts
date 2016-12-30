8d44976e9e8e5599041a79a17dc1745950f35334
jjusianiec <jjusianiec@gmail.com>
2016-12-30 20:51:55 +0100

    Try to connect to db before ace initialize

--- src/www.ts
+++ src/www.ts
@@ -1,5 +1,20 @@
 "use strict";
-require('pg').defaults.ssl = true;
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function(row) {
+            console.log(JSON.stringify(row));
+        });
+});
+
+
 var server = require("./app");
 var app = server.app;
 var addon = server.addon;
